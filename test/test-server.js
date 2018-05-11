b099e0f1aeb7242690aa94b63bcb7b42475c3d95
Kevin Llorente <kevin.llorente@gmail.com>
2018-05-11 13:44:24 -0700

    test fail fixes

--- test/test-server.js
+++ test/test-server.js
@@ -5,6 +5,8 @@ const {app, runServer, closeServer} = require('../server');
 
 const expect = chai.expect; 
 
+chai.use(chaiHTTP); 
+
 describe('Blog Posts', function () {
 
 	before(function() {
@@ -19,7 +21,7 @@ describe('Blog Posts', function () {
 		return chai.request(app)
 		.get('/blog-posts')
 		.then(function(res) {
-			expect(res)to.have.status(200); 
+			expect(res).to.have.status(200); 
 			expect(res).to.be.json;
 			expect(res.body).to.be.a('array');
 			expect(res.body.length).to.be.at.least(1); 
@@ -33,17 +35,19 @@ describe('Blog Posts', function () {
 	});
 
 	it ('should add item on POST', function () {
-		const newItem = {title: 'Cars', content: 'Cars are fast and valuable', author:'Harambe'};
+		const newItem = {title: 'Cars', content: 'Cars are fast and valuable', author: 'Harambe'};
 		return chai.request(app)
 		.post('/blog-posts')
 		.send(newItem)
 		.then(function(res) {
 			expect(res).to.have.status(201); 
 			expect(res).to.be.json;
-			expect(res.body)to.be.a('object');
-			expect(res.body)to.include.keys('id', 'title', 'content', 'author'); 
+			expect(res.body).to.be.a('object');
+			expect(res.body).to.include.keys('id', 'title', 'content', 'author'); 
 			expect(res.body.id).to.not.equal(null); 
-			expect(res.body).to.deep.equal(Object.assign(newItem, {id: res.body.id})); 
+			expect(res.body.title).to.equal(newItem.title); 
+			expect(res.body.content).to.equal(newItem.content)
+			expect(res.body.author).to.equal(newItem.author)
 		});
 	}); 
 
